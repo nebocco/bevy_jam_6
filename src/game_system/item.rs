63f17a20caebecf88a094152d34e319f2bb880d4
nebocco <nebocco@gmail.com>
2025-06-04 22:45:40 +0900

    アイテム管理システムを強化し、オブジェクト作成機能を追加。グリッド上でのアイテム生成とリセット機能を実装。

--- src/game_system/item.rs
+++ src/game_system/item.rs
@@ -17,14 +17,7 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Gameplay), spawn_item_buttons);
 }
 
-fn spawn_item_buttons(
-    mut commands: Commands,
-    item_assets: Res<ItemAssets>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    let layout = TextureAtlasLayout::from_grid(UVec2::splat(32), 4, 8, Some(UVec2::splat(1)), None);
-    let texture_atlas_layout = texture_atlas_layouts.add(layout);
-
+fn spawn_item_buttons(mut commands: Commands, item_assets: Res<ItemAssets>) {
     commands
         .spawn((
             widget::ui_root("Item Buttons"),
@@ -33,21 +26,27 @@ fn spawn_item_buttons(
             children![
                 widget::item_button(
                     Handle::clone(&item_assets.sprite_sheet),
-                    Handle::clone(&texture_atlas_layout),
+                    Handle::clone(&item_assets.texture_atlas_layout),
                     0,
                     select_item::<0>
                 ),
                 widget::item_button(
                     Handle::clone(&item_assets.sprite_sheet),
-                    Handle::clone(&texture_atlas_layout),
+                    Handle::clone(&item_assets.texture_atlas_layout),
                     1,
                     select_item::<1>
                 ),
                 widget::item_button(
                     Handle::clone(&item_assets.sprite_sheet),
-                    Handle::clone(&texture_atlas_layout),
+                    Handle::clone(&item_assets.texture_atlas_layout),
                     2,
                     select_item::<2>
+                ),
+                widget::item_button(
+                    Handle::clone(&item_assets.sprite_sheet),
+                    Handle::clone(&item_assets.texture_atlas_layout),
+                    8,
+                    select_item::<255> // Eraser
                 ),
             ],
             BackgroundColor(Color::WHITE),
@@ -64,15 +63,16 @@ fn spawn_item_buttons(
         });
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-enum Item {
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub(super) enum Item {
     Item1,
     Item2,
     Item3,
+    Eraser,
 }
 
 #[derive(Resource, Debug, Clone, Copy, Default)]
-struct SelectedItem(Option<Item>);
+pub(super) struct SelectedItem(pub Option<Item>);
 
 impl From<u8> for Item {
     fn from(value: u8) -> Self {
@@ -80,6 +80,7 @@ impl From<u8> for Item {
             0 => Item::Item1,
             1 => Item::Item2,
             2 => Item::Item3,
+            255 => Item::Eraser,
             _ => panic!("Invalid item index"),
         }
     }
@@ -100,13 +101,24 @@ fn select_item<const I: u8>(_: Trigger<Pointer<Click>>, mut selected_item: ResMu
 #[reflect(Resource)]
 pub struct ItemAssets {
     #[dependency]
-    sprite_sheet: Handle<Image>,
+    pub sprite_sheet: Handle<Image>,
     // #[dependency]
     // pub steps: Vec<Handle<AudioSource>>,
+    pub texture_atlas_layout: Handle<TextureAtlasLayout>,
 }
 
 impl FromWorld for ItemAssets {
     fn from_world(world: &mut World) -> Self {
+        let texture_atlas_layout = {
+            let mut texture_atlas = world.resource_mut::<Assets<TextureAtlasLayout>>();
+            texture_atlas.add(TextureAtlasLayout::from_grid(
+                UVec2::splat(32),
+                4,
+                8,
+                Some(UVec2::splat(1)),
+                None,
+            ))
+        };
         let assets = world.resource::<AssetServer>();
         Self {
             sprite_sheet: assets.load_with_settings(
@@ -122,6 +134,7 @@ impl FromWorld for ItemAssets {
             //     assets.load("audio/sound_effects/step3.ogg"),
             //     assets.load("audio/sound_effects/step4.ogg"),
             // ],
+            texture_atlas_layout,
         }
     }
 }
--- src/game_system/level.rs
+++ src/game_system/level.rs
@@ -2,15 +2,32 @@
 
 use std::fmt::Debug;
 
-use bevy::{color::palettes, ecs::relationship::RelatedSpawnerCommands, prelude::*};
+use bevy::{
+    color::palettes,
+    ecs::relationship::RelatedSpawnerCommands,
+    input::{ButtonState, keyboard::KeyboardInput},
+    prelude::*,
+};
 
-use crate::{asset_tracking::LoadResource, audio::music, screens::Screen};
+use crate::{
+    asset_tracking::LoadResource,
+    audio::music,
+    game_system::item::{self, Item},
+    screens::Screen,
+};
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<LevelAssets>();
-    app.load_resource::<LevelAssets>();
-    app.add_plugins(MeshPickingPlugin);
-    app.add_systems(OnEnter(Screen::Gameplay), spawn_level);
+    app.register_type::<LevelAssets>()
+        .load_resource::<LevelAssets>()
+        .init_resource::<ObjectMap>();
+    app.add_plugins(MeshPickingPlugin)
+        .add_event::<CreateObject>();
+    app.add_systems(OnEnter(Screen::Gameplay), spawn_level)
+        .add_observer(create_object)
+        .add_systems(
+            Update,
+            (reset_all_object_placements,).run_if(in_state(Screen::Gameplay)),
+        );
 }
 
 #[derive(Resource, Asset, Clone, Reflect)]
@@ -27,6 +44,24 @@ impl FromWorld for LevelAssets {
             music: assets.load("audio/music/Fluffing A Duck.ogg"),
         }
     }
+}
+
+#[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct GridCoord {
+    pub x: u8,
+    pub y: u8,
+}
+
+#[derive(Debug, Clone, Copy, Event)]
+struct CreateObject {
+    parent_grid: Entity,
+    pub coord: GridCoord,
+    item: item::Item,
+}
+
+#[derive(Resource, Debug, Clone, Default)]
+struct ObjectMap {
+    objects: std::collections::HashMap<GridCoord, (item::Item, Entity)>,
 }
 
 /// A system that spawns the main level.
@@ -69,13 +104,14 @@ pub fn spawn_grid(
             StateScoped(Screen::Gameplay),
         ))
         .with_children(move |parent| {
-    for x in 0..10 {
-        for y in 0..10 {
-            let color_handle = Handle::clone(&color_handle);
-            let hovered_color_handle = Handle::clone(&hovered_color_handle);
-            parent
+            for x in 0..10 {
+                for y in 0..10 {
+                    let color_handle = Handle::clone(&color_handle);
+                    let hovered_color_handle = Handle::clone(&hovered_color_handle);
+                    parent
                         .spawn((
                             Name::new(format!("Tile ({}, {})", x, y)),
+                            GridCoord { x, y },
                             Transform::from_xyz(x as f32 * 32.0 - 144.0, y as f32 * 32.0 - 144.0, 0.0),
                             Pickable::default(),
                             Mesh2d(Handle::clone(&rect_handle)),
@@ -83,23 +119,38 @@ pub fn spawn_grid(
                         ))
                         .observe(
                             move |over: Trigger<Pointer<Over>>,
-                             mut color: Query<&mut MeshMaterial2d<ColorMaterial>>,|{
-                                println!("Hovered over tile ({}, {})", x, y);
+                                mut color: Query<&mut MeshMaterial2d<ColorMaterial>>,|{
                                 let mut color = color.get_mut(over.target()).unwrap();
                                 color.0 = Handle::clone(&hovered_color_handle);
                             },
                         ).observe(
                             move |out: Trigger<Pointer<Out>>,
-                             mut color: Query<&mut MeshMaterial2d<ColorMaterial>>,|{
-                                println!("Pointer left tile ({}, {})", x, y);
+                                mut color: Query<&mut MeshMaterial2d<ColorMaterial>>,|{
                                 let mut color = color.get_mut(out.target()).unwrap();
                                 color.0 = Handle::clone(&color_handle);
                             },
+                        ).observe(
+                            |out: Trigger<Pointer<Pressed>>,
+                                coord: Query<&GridCoord>,
+                                selected_item: Res<item::SelectedItem>,
+                                mut commands: Commands| {
+                                let entity = out.target();
+                                let &coord = coord.get(entity).unwrap();
+                                println!("Creating object at coord: {:?}", coord);
+                                let Some(item) = selected_item.0 else {
+                                    return;
+                                };
+                                println!("Creating object with item: {:?}", item);
+                                commands.trigger(CreateObject {
+                                    parent_grid: entity,
+                                    coord,
+                                    item,
+                                });
+                            }
                         );
-
-        }
-    }
-    });
+                }
+            }
+        });
 }
 
 // An observer listener that changes the target entity's color.
@@ -109,5 +160,54 @@ fn recolor_on<E: Debug + Clone + Reflect>(color: Color) -> impl Fn(Trigger<E>, Q
             return;
         };
         sprite.color = color;
+    }
+}
+
+fn reset_all_object_placements(
+    button_input: Res<ButtonInput<KeyCode>>,
+    mut commands: Commands,
+    mut object_map: ResMut<ObjectMap>,
+) {
+    if button_input.just_pressed(KeyCode::KeyR) {
+        for (_key, (_item, entity)) in object_map.objects.drain() {
+            commands.entity(entity).despawn();
+        }
+    }
+}
+
+// create item on grid click
+fn create_object(
+    trigger: Trigger<CreateObject>,
+    mut commands: Commands,
+    item_assets: Res<item::ItemAssets>,
+    mut object_map: ResMut<ObjectMap>,
+) {
+    let event = trigger.event();
+    println!("Creating object at coord: {:?}", event.coord);
+    println!("{:?}", &object_map.objects);
+
+    if let Some((_, existing_entity)) = object_map.objects.remove(&event.coord) {
+        commands.entity(existing_entity).despawn();
+    }
+
+    if event.item != item::Item::Eraser {
+        let entity = commands
+            .spawn((
+                Name::new("Item Object"),
+                GridCoord::clone(&event.coord),
+                Item::clone(&event.item),
+                Sprite::from_atlas_image(
+                    item_assets.sprite_sheet.clone(),
+                    TextureAtlas {
+                        layout: item_assets.texture_atlas_layout.clone(),
+                        index: event.item as usize,
+                    },
+                ),
+                StateScoped(Screen::Gameplay),
+            ))
+            .id();
+
+        commands.entity(event.parent_grid).add_child(entity);
+        object_map.objects.insert(event.coord, (event.item, entity));
     }
 }
