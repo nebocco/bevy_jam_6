733d9e4f5cfd2da863c196f73614bd5ea7797005
nebocco <nebocco@gmail.com>
2025-06-09 22:40:38 +0900

    ポーズメニューの「続ける」ボタンを「タイトルに戻る」ボタンに変更し、レベル選択画面での動作を追加

--- src/menus/pause.rs
+++ src/menus/pause.rs
@@ -16,18 +16,38 @@ pub(super) fn plugin(app: &mut App) {
     );
 }
 
-fn spawn_pause_menu(mut commands: Commands, ui_assets: Res<UiAssets>) {
-    commands.spawn((
-        widget::ui_root("Pause Menu"),
-        GlobalZIndex(2),
-        StateScoped(Menu::Pause),
-        children![
-            widget::header("Game paused", Handle::clone(&ui_assets.font)),
-            widget::text_button("Continue", &ui_assets, close_menu),
-            widget::text_button("Settings", &ui_assets, open_settings_menu),
-            widget::text_button("Select Level", &ui_assets, back_to_level_select),
-        ],
-    ));
+fn spawn_pause_menu(
+    mut commands: Commands,
+    ui_assets: Res<UiAssets>,
+    current_screen: Res<State<Screen>>,
+) {
+    if current_screen.get() != &Screen::LevelSelect {
+        // If the current screen is not LevelSelect, we don't spawn the pause menu.
+        commands.spawn((
+            widget::ui_root("Pause Menu"),
+            GlobalZIndex(2),
+            StateScoped(Menu::Pause),
+            children![
+                widget::header("Game paused", Handle::clone(&ui_assets.font)),
+                widget::text_button("Resume", &ui_assets, close_menu),
+                widget::text_button("Settings", &ui_assets, open_settings_menu),
+                widget::text_button("Select Level", &ui_assets, back_to_level_select),
+            ],
+        ));
+    } else {
+        // If the current screen is LevelSelect, we spawn a different pause menu.
+        commands.spawn((
+            widget::ui_root("Pause Menu"),
+            GlobalZIndex(2),
+            StateScoped(Menu::Pause),
+            children![
+                widget::header("Game paused", Handle::clone(&ui_assets.font)),
+                widget::text_button("Resume", &ui_assets, close_menu),
+                widget::text_button("Settings", &ui_assets, open_settings_menu),
+                widget::text_button("Quit to title", &ui_assets, quit_to_title),
+            ],
+        ));
+    }
 }
 
 fn open_settings_menu(_: Trigger<Pointer<Click>>, mut next_menu: ResMut<NextState<Menu>>) {
@@ -40,6 +60,10 @@ fn close_menu(_: Trigger<Pointer<Click>>, mut next_menu: ResMut<NextState<Menu>>
 
 fn back_to_level_select(_: Trigger<Pointer<Click>>, mut next_screen: ResMut<NextState<Screen>>) {
     next_screen.set(Screen::LevelSelect);
+}
+
+fn quit_to_title(_: Trigger<Pointer<Click>>, mut next_screen: ResMut<NextState<Screen>>) {
+    next_screen.set(Screen::Title);
 }
 
 fn go_back(mut next_menu: ResMut<NextState<Menu>>) {
